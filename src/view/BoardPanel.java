546b97101a72436da3f302559e11b84a3ab155bb
Gannon Wooley <gannonwooley@gmail.com>
2025-11-19 16:21:11 -0500

    Updated click ability to fill cell with color, made a temporary board for testing which will be replaced by backend board.

--- src/view/BoardPanel.java
+++ src/view/BoardPanel.java
@@ -8,9 +8,17 @@ public class BoardPanel extends JPanel {
 	
 	private final int cellSize = 40;
 	private final int gridSize = 10;
-	
+    private final int[][] cellStates;
+
 	public BoardPanel() {
         setPreferredSize(new Dimension(cellSize * gridSize, cellSize * gridSize));
+
+        cellStates = new int[gridSize][gridSize];
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                cellStates[i][j] = 0;
+            }
+        }
 
         //mouse listener
         addMouseListener(new MouseAdapter() {
@@ -20,7 +28,18 @@ public class BoardPanel extends JPanel {
                 int row = e.getY() / cellSize;
                 int col = e.getX() / cellSize;
 
-                System.out.println("Clicked cell: (" + row + ", " + col + ")");
+                //out of bounds check
+                if(row >= 0 && row < gridSize && col >= 0 && col < gridSize) {
+
+                    if (cellStates[row][col] == 0) {
+                        cellStates[row][col] = 1;
+                    } else {
+                        cellStates[row][col] = 0;
+                    }
+
+                    System.out.println("Clicked cell: (" + row + ", " + col + ")");
+                    repaint();
+                }
             }
         });
     }
@@ -28,6 +47,21 @@ public class BoardPanel extends JPanel {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+
+        //fill cells based on state
+        for(int i = 0; i < gridSize; i++) {
+            for(int j = 0; j < gridSize; j++) {
+                int x = j *  cellSize;
+                int y = i * cellSize;
+
+                if(cellStates[i][j] == 1) {
+                    g.setColor(Color.BLUE);
+                } else {
+                    g.setColor(Color.WHITE);
+                }
+                g.fillRect(x, y, cellSize, cellSize);
+            }
+        }
 		
 		//draws vertical and horizontal lines
 		g.setColor(Color.BLACK);
